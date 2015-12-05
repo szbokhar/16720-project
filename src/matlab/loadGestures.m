5b3c94825d9e463235b51ea4185c2025a0e0ce0b
Zahir Bokhari <syedzahirbokhari@gmail.com>
2015-12-05 00:36:30 -0500

    Update matlab HMM scripts to train and display 4 models

--- src/matlab/loadGestures.m
+++ src/matlab/loadGestures.m
@@ -1,6 +1,7 @@
 g1 = {}
 g2 = {}
 g3 = {}
+g4 = {}
 seq_g1_1 = {}
 seq_g2_1 = {}
 seq_g3_1 = {}
@@ -10,15 +11,22 @@ seq_g3_2 = {}
 seq_g1_3 = {}
 seq_g2_3 = {}
 seq_g3_3 = {}
+seq_g4 = {}
 G1 = []
 G2 = []
 G3 = []
+G4 = []
 N1 = 20
 N2 = 20
 N3 = 20
+N4 = 53
 M1 = 10
 M2 = 10
 M3 = 10
+Mall = 20
+M1 = Mall
+M2 = Mall
+M3 = Mall
 
 for i=1:N1
     g1{i} = load(sprintf('data/ges1_%g.txt', i));
@@ -35,20 +43,18 @@ for i=1:N3
     G3 = [G3; g3{i}];
 end
 
-A = G1;
-[clus1,C1] = kmeans(A(:, 3:4), M1)
-figure(1)
-plot(G1(:,3), -G1(:,4), 'r.'); hold on; plot(C1(:,1), -C1(:,2), 'b*'); axis([-60 20 -60 20]); hold off
+for i=1:N4
+    g4{i} = load(sprintf('data/gesNon_%g.txt', i));
+    G4 = [G4; g4{i}];
+end
 
-A = G2;
-[clus2,C2] = kmeans(A(:, 3:4), M2)
-figure(2)
-plot(G2(:,3), -G2(:,4), 'r.'); hold on; plot(C2(:,1), -C2(:,2), 'b*'); axis([-40 100 -40 100]); hold off
-
-A = G3;
-[clus3,C3] = kmeans(A(:, 3:4), M3)
-figure(3)
-plot(G3(:,3), -G3(:,4), 'r.'); hold on; plot(C3(:,1), -C3(:,2), 'b*'); axis([-50 10 -50 10]); hold off
+A = [G1; G2; G3; G4];
+[clusAll,C] = kmeans(A(:, 3:4), Mall)
+figure(4)
+plot(A(:,3), -A(:,4), 'r.'); hold on; plot(C(:,1), -C(:,2), 'b*'); hold off
+C1 = C;
+C2 = C;
+C3 = C;
 
 %{
 A = load('data/obsdata.txt');
@@ -83,15 +89,20 @@ for i=1:N3
     seq_g3_3{i} = I;
 end
 
+for i=1:N4
+    [~, I] = min(pdist2(g4{i}(:, 3:4), C)');
+    seq_g4{i} = I;
+end
+
 K = 4;
 a0 = eye(K) + circshift(eye(K), [0,1]); a0(end,1)=0; a0 = bsxfun(@rdivide, a0, sum(a0, 2));
 b0 = rand(K,M1)/M1; b0 = bsxfun(@rdivide, b0, sum(b0, 2));
 p0 = zeros(K,1);
 p0(1) = 1;
 
-[a1, b1, p1] = EM_estimate(a0, b0, p0, seq_g1_1(1:15), 700)
+[a1, b1, p1] = EM_estimate(a0, b0, p0, seq_g1_1, 700)
 
-figure(4)
+figure(5)
 for i=1:K
     subplot(1,K,i)
     plot(G1(:,3), -G1(:,4), 'g.');
@@ -107,9 +118,9 @@ b0 = rand(K,M2)/M2; b0 = bsxfun(@rdivide, b0, sum(b0, 2));
 p0 = zeros(K,1);
 p0(1) = 1;
 
-[a2, b2, p2] = EM_estimate(a0, b0, p0, seq_g2_2(1:15), 700)
+[a2, b2, p2] = EM_estimate(a0, b0, p0, seq_g2_2, 700)
 
-figure(5)
+figure(6)
 for i=1:K
     subplot(K,1,i)
     plot(G2(:,3), -G2(:,4), 'g.');
@@ -125,9 +136,9 @@ b0 = rand(K,M3)/M3; b0 = bsxfun(@rdivide, b0, sum(b0, 2));
 p0 = zeros(K,1);
 p0(1) = 1;
 
-[a3, b3, p3] = EM_estimate(a0, b0, p0, seq_g3_3(1:15), 700)
+[a3, b3, p3] = EM_estimate(a0, b0, p0, seq_g3_3, 700)
 
-figure(6)
+figure(7)
 for i=1:K
     subplot(K,1,i)
     plot(G3(:,3), -G3(:,4), 'g.');
@@ -137,12 +148,31 @@ for i=1:K
     hold off
 end
 
+K = 4;
+a0 = eye(K) + circshift(eye(K), [0,1]); a0(end,1)=0; a0 = bsxfun(@rdivide, a0, sum(a0, 2));
+b0 = rand(K,Mall)/Mall; b0 = bsxfun(@rdivide, b0, sum(b0, 2));
+p0 = zeros(K,1);
+p0(1) = 1;
+
+[a4, b4, p4] = EM_estimate(a0, b0, p0, [seq_g4], 700)
+
+figure(8)
+for i=1:K
+    subplot(K,1,i)
+    plot(A(:,3), -A(:,4), 'g.');
+    hold on;
+    scatter(C(:,1), -C(:,2), 1000*b3(i,:)', [0,0,0]);
+    hold off
+end
+
 disp('gesture 1')
 for i=1:N1
     l11 = likelihood(a1,b1,p1,seq_g1_1(i));
     l12 = likelihood(a2,b2,p2,seq_g1_2(i));
     l13 = likelihood(a3,b3,p3,seq_g1_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l11, l12, l13))
+    l14 = likelihood(a4,b4,p4,seq_g1_3(i));
+    [~,j] = max([l11,l12,l13,l14]);
+    disp(sprintf('%g >> %g \t %g \t %g \t %g \t %g', i, l11, l12, l13, l14, j))
 end
 
 disp('gesture 2')
@@ -150,7 +180,9 @@ for i=1:N2
     l21 = likelihood(a1,b1,p1,seq_g2_1(i));
     l22 = likelihood(a2,b2,p2,seq_g2_2(i));
     l23 = likelihood(a3,b3,p3,seq_g2_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l21, l22, l23))
+    l24 = likelihood(a4,b4,p4,seq_g2_3(i));
+    [~,j] = max([l21,l22,l23,l24]);
+    disp(sprintf('%g >> %g \t %g \t %g \t %g \t %g', i, l21, l22, l23, l24, j))
 end
 
 disp('gesture 3')
@@ -158,7 +190,19 @@ for i=1:N3
     l31 = likelihood(a1,b1,p1,seq_g3_1(i));
     l32 = likelihood(a2,b2,p2,seq_g3_2(i));
     l33 = likelihood(a3,b3,p3,seq_g3_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l31, l32, l33))
+    l34 = likelihood(a4,b4,p4,seq_g3_3(i));
+    [~,j] = max([l31,l32,l33,l34]);
+    disp(sprintf('%g >> %g \t %g \t %g \t %g \t %g', i, l31, l32, l33, l34, j))
+end
+
+disp('No gesture')
+for i=1:N4
+    l41 = likelihood(a1,b1,p1,seq_g4(i));
+    l42 = likelihood(a2,b2,p2,seq_g4(i));
+    l43 = likelihood(a3,b3,p3,seq_g4(i));
+    l44 = likelihood(a4,b4,p4,seq_g4(i));
+    [~,j] = max([l41,l42,l43,l44]);
+    disp(sprintf('%g >> %g \t %g \t %g \t %g \t %g', i, l41, l42, l43, l44, j))
 end
 
 
--- src/matlab/runGestureLiklihoodScript.m
+++ src/matlab/runGestureLiklihoodScript.m
@@ -1,24 +1,35 @@
 g1 = {}
 g2 = {}
 g3 = {}
+g4 = {}
 seq_g1_1 = {}
 seq_g2_1 = {}
 seq_g3_1 = {}
+seq_g4_1 = {}
 seq_g1_2 = {}
 seq_g2_2 = {}
 seq_g3_2 = {}
+seq_g4_2 = {}
 seq_g1_3 = {}
 seq_g2_3 = {}
 seq_g3_3 = {}
+seq_g4_3 = {}
+seq_g1_4 = {}
+seq_g2_4 = {}
+seq_g3_4 = {}
+seq_g4_4 = {}
 G1 = []
 G2 = []
 G3 = []
+G4 = []
 N1 = 20
 N2 = 20
 N3 = 20
+N4 = 17
 M1 = 10
 M2 = 10
 M3 = 10
+M4 = 20
 
 for i=1:N1
     g1{i} = load(sprintf('data/ges1_%g.txt', i));
@@ -35,17 +46,27 @@ for i=1:N3
     G3 = [G3; g3{i}];
 end
 
-C1 = csvread('../models/UD_obs.csv');
+for i=1:N4
+    g4{i} = load(sprintf('data/gesNon_%g.txt', i));
+    G4 = [G4; g4{i}];
+end
+
+C1 = csvread('../modelsTest/gpUD_obs.csv');
 figure(1)
 plot(G1(:,3), -G1(:,4), 'r.'); hold on; plot(C1(:,1), -C1(:,2), 'b*'); axis([-60 20 -60 20]); hold off
 
-C2 = csvread('../models/LL_obs.csv');
+C2 = csvread('../modelsTest/gpLL_obs.csv');
 figure(2)
 plot(G2(:,3), -G2(:,4), 'r.'); hold on; plot(C2(:,1), -C2(:,2), 'b*'); axis([-40 100 -40 100]); hold off
 
-C3 = csvread('../models/LR_obs.csv');
+C3 = csvread('../modelsTest/gpLR_obs.csv');
 figure(3)
 plot(G3(:,3), -G3(:,4), 'r.'); hold on; plot(C3(:,1), -C3(:,2), 'b*'); axis([-50 10 -50 10]); hold off
+
+A = G4;
+[clus4,C4] = kmeans(A(:, 3:4), M4)
+figure(4)
+plot(G4(:,3), -G4(:,4), 'r.'); hold on; plot(C4(:,1), -C4(:,2), 'b*'); axis([-50 50 -50 50]); hold off
 
 for i=1:N1
     [~, I] = min(pdist2(g1{i}(:, 3:4), C1)');
@@ -54,6 +75,8 @@ for i=1:N1
     seq_g1_2{i} = I;
     [~, I] = min(pdist2(g1{i}(:, 3:4), C3)');
     seq_g1_3{i} = I;
+    [~, I] = min(pdist2(g1{i}(:, 3:4), C4)');
+    seq_g1_4{i} = I;
 end
 
 for i=1:N2
@@ -63,6 +86,8 @@ for i=1:N2
     seq_g2_2{i} = I;
     [~, I] = min(pdist2(g2{i}(:, 3:4), C3)');
     seq_g2_3{i} = I;
+    [~, I] = min(pdist2(g2{i}(:, 3:4), C4)');
+    seq_g2_4{i} = I;
 end
 
 for i=1:N3
@@ -72,14 +97,27 @@ for i=1:N3
     seq_g3_2{i} = I;
     [~, I] = min(pdist2(g3{i}(:, 3:4), C3)');
     seq_g3_3{i} = I;
+    [~, I] = min(pdist2(g3{i}(:, 3:4), C4)');
+    seq_g3_4{i} = I;
+end
+
+for i=1:N4
+    [~, I] = min(pdist2(g4{i}(:, 3:4), C1)');
+    seq_g4_1{i} = I;
+    [~, I] = min(pdist2(g4{i}(:, 3:4), C2)');
+    seq_g4_2{i} = I;
+    [~, I] = min(pdist2(g4{i}(:, 3:4), C3)');
+    seq_g4_3{i} = I;
+    [~, I] = min(pdist2(g4{i}(:, 3:4), C4)');
+    seq_g4_4{i} = I;
 end
 
 K = 4;
-a1 = csvread('../models/UD_a.csv');
-b1 = csvread('../models/UD_b.csv');
-p1 = csvread('../models/UD_p.csv');
+a1 = csvread('../modelsTest/gpUD_a.csv');
+b1 = csvread('../modelsTest/gpUD_b.csv');
+p1 = csvread('../modelsTest/gpUD_p.csv');
 
-figure(4)
+figure(5)
 for i=1:K
     subplot(1,K,i)
     plot(G1(:,3), -G1(:,4), 'g.');
@@ -90,11 +128,11 @@ for i=1:K
 end
 
 K = 6;
-a2 = csvread('../models/LL_a.csv');
-b2 = csvread('../models/LL_b.csv');
-p2 = csvread('../models/LL_p.csv');
+a2 = csvread('../modelsTest/gpLL_a.csv');
+b2 = csvread('../modelsTest/gpLL_b.csv');
+p2 = csvread('../modelsTest/gpLL_p.csv');
 
-figure(5)
+figure(6)
 for i=1:K
     subplot(K,1,i)
     plot(G2(:,3), -G2(:,4), 'g.');
@@ -105,11 +143,11 @@ for i=1:K
 end
 
 K = 6;
-a3 = csvread('../models/LR_a.csv');
-b3 = csvread('../models/LR_b.csv');
-p3 = csvread('../models/LR_p.csv');
+a3 = csvread('../modelsTest/gpLR_a.csv');
+b3 = csvread('../modelsTest/gpLR_b.csv');
+p3 = csvread('../modelsTest/gpLR_p.csv');
 
-figure(6)
+figure(7)
 for i=1:K
     subplot(K,1,i)
     plot(G3(:,3), -G3(:,4), 'g.');
@@ -119,12 +157,30 @@ for i=1:K
     hold off
 end
 
+K = 6;
+a0 = eye(K) + circshift(eye(K), [0,1]); a0(end,1)=0; a0 = bsxfun(@rdivide, a0, sum(a0, 2));
+b0 = rand(K,M4)/M4; b0 = bsxfun(@rdivide, b0, sum(b0, 2));
+p0 = zeros(K,1);
+p0(1) = 1;
+
+[a4, b4, p4] = EM_estimate(a0, b0, p0, seq_g4_4(1:15), 700)
+
+figure(8)
+for i=1:K
+    subplot(K,1,i)
+    plot(G4(:,3), -G4(:,4), 'g.');
+    hold on;
+    scatter(C4(:,1), -C4(:,2), 1000*b4(i,:)', [0,0,0]);
+    hold off
+end
+
 disp('gesture 1')
 for i=1:N1
     l11 = likelihood(a1,b1,p1,seq_g1_1(i));
     l12 = likelihood(a2,b2,p2,seq_g1_2(i));
     l13 = likelihood(a3,b3,p3,seq_g1_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l11, l12, l13))
+    l14 = likelihood(a4,b4,p4,seq_g1_4(i));
+    disp(sprintf('%g >> %g \t %g \t %g \t %g', i, l11, l12, l13, l14))
 end
 
 disp('gesture 2')
@@ -132,7 +188,8 @@ for i=1:N2
     l21 = likelihood(a1,b1,p1,seq_g2_1(i));
     l22 = likelihood(a2,b2,p2,seq_g2_2(i));
     l23 = likelihood(a3,b3,p3,seq_g2_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l21, l22, l23))
+    l24 = likelihood(a4,b4,p4,seq_g2_4(i));
+    disp(sprintf('%g >> %g \t %g \t %g \t %g', i, l21, l22, l23, l24))
 end
 
 disp('gesture 3')
@@ -140,9 +197,18 @@ for i=1:N3
     l31 = likelihood(a1,b1,p1,seq_g3_1(i));
     l32 = likelihood(a2,b2,p2,seq_g3_2(i));
     l33 = likelihood(a3,b3,p3,seq_g3_3(i));
-    disp(sprintf('%g >> %g \t %g \t %g', i, l31, l32, l33))
+    l34 = likelihood(a4,b4,p4,seq_g3_4(i));
+    disp(sprintf('%g >> %g \t %g \t %g \t %g', i, l31, l32, l33, l34))
 end
 
+disp('No gesture')
+for i=1:N4
+    l41 = likelihood(a1,b1,p1,seq_g3_1(i));
+    l42 = likelihood(a2,b2,p2,seq_g3_2(i));
+    l43 = likelihood(a3,b3,p3,seq_g3_3(i));
+    l44 = likelihood(a4,b4,p4,seq_g3_4(i));
+    disp(sprintf('%g >> %g \t %g \t %g \t %g', i, l41, l42, l43, l44))
+end
 
 %{
 disp('others')
